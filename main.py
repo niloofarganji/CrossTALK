47e0d723b35f67834f82da891e58cab76c59ae4b
Niloofar Ganji <niloofar.ganji@sickkids.ca>
2025-06-18 11:13:10 -0400

    baseline models LR, LightGBM, Random forest

--- main.py
+++ main.py
@@ -7,7 +7,18 @@ from src.crosstalk import train
 
 CONFIG = {
     # Name for this specific run, used to create the output folder.
-    'RUN_NAME': 'baseline_logistic_regression',
+    'RUN_NAME': 'lightgbm_all_connectivity_fps',
+
+    # The model architecture to use for this experiment.
+    # Options: 'logistic_regression', 'random_forest', 'lightgbm'
+    'MODEL_NAME': 'lightgbm',
+
+    # Model-specific parameters. These will be passed to the model's constructor.
+    'MODEL_PARAMS': {
+        'n_estimators': 500,  # More trees can be beneficial for gradient boosting
+        'learning_rate': 0.05, # A smaller learning rate often improves accuracy
+        'num_leaves': 31,     # Default value, good starting point
+    },
 
     # Base directory where all experiment results will be saved.
     'EXPORT_BASE_DIR': 'Exports',
@@ -15,16 +26,19 @@ CONFIG = {
     # Path to the training data.
     'DATA_PATH': os.path.join('data', 'crosstalk_train (2).parquet'),
     
-    # List of fingerprint columns to use as features.
-    'FEATURES': ['ECFP4'],
+    # List of fingerprint columns to use.
+    'FINGERPRINT_FEATURES': ['ECFP4', 'ECFP6', 'FCFP4', 'FCFP6'],
+
+    # List of numeric columns to use.
+    'NUMERIC_FEATURES': ['MW', 'ALOGP'],
     
     # Name of the column containing the labels.
     'LABEL': 'DELLabel',
     
     # Proportion of data to be used for the validation set.
-    'TEST_SIZE': 0.2,
+    'TEST_SIZE': 0.2, # Using a smaller validation set to maximize training data
     
-    # Number of rows to load for a quick test. Set to None to load all data.
+    # Set to None to run on the entire dataset.
     'MAX_ROWS': None,
     
     # Filename for the saved model artifact within the run's output directory.
