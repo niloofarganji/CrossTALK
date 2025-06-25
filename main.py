95c0345c5402a6a44684aa96e9e03e63f6616d03
Niloofar Ganji <niloofar.ganji@sickkids.ca>
2025-06-25 08:55:34 -0400

    config for best hyperparameters of xgboost & catboost

--- main.py
+++ main.py
@@ -7,17 +7,33 @@ from src.crosstalk import train
 
 CONFIG = {
     # Name for this specific run, used to create the output folder.
-    'RUN_NAME': 'catboost_ECFP6_FCFP6_TOPTOR_ATOMPAIR',
+    'RUN_NAME': 'catboost_prob_paramtuned_ECFP6_ATOMPAIR_non_numeric',
 
     # The model architecture to use for this experiment.
     # Options: 'logistic_regression', 'random_forest', 'lightgbm'
     'MODEL_NAME': 'catboost',
 
     # Model-specific parameters. These will be passed to the model's constructor.
+    #after hyperparameter tuning
+    #from Exports/Hyperparameter_Tuning/xgboost_paramtuned_ECFP6_FCFP6_TOPTOR_ATOMPAIR_20250621-143003/best_params.json
+    
+    #for xgboost: 
+    #'MODEL_PARAMS': {
+    #    'learning_rate': 0.1,
+    #    'max_depth': 7,
+    #    'n_estimators': 500
+    #},
+    
     'MODEL_PARAMS': {
-        'n_estimators': 500,      # Number of trees to build
-        'learning_rate': 0.05,    # Step size shrinkage
-        'max_depth': 6,           # Depth of the trees (CatBoost uses max_depth instead of num_leaves)
+        "learning_rate": 0.2141837713010419,
+        "depth": 10,
+        "l2_leaf_reg": 1.261357543385574,
+        "random_strength": 0.0005881339039579889
+    },
+
+    #Set Hyperparameter Tuning to False
+    'HYPERPARAM_TUNING': {
+        'enabled': False
     },
 
     # Base directory where all experiment results will be saved.
@@ -27,10 +43,10 @@ CONFIG = {
     'DATA_PATH': os.path.join('data', 'crosstalk_train (2).parquet'),
     
     # List of fingerprint columns to use.
-    'FINGERPRINT_FEATURES': ['ECFP6', 'FCFP6', 'TOPTOR', 'ATOMPAIR'],
+    'FINGERPRINT_FEATURES': ['ECFP6', 'ATOMPAIR'],
 
     # List of numeric columns to use.
-    'NUMERIC_FEATURES': ['MW', 'ALOGP'],
+    'NUMERIC_FEATURES': [],
     
     # Name of the column containing the labels.
     'LABEL': 'DELLabel',
@@ -41,7 +57,7 @@ CONFIG = {
     # --- Threshold Optimization ---
     # Set to True to analyze precision-recall trade-offs at different thresholds.
     'THRESHOLD_OPTIMIZATION': {
-        'enabled': True
+        'enabled': False
     },
 
     # Set to None to run on the entire dataset.
